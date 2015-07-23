0f8f62a373e6b963d2444959caf30165f86a5c0c
mar <mar.nospam@anomy.net>
2015-07-23 12:21:20 +0000

    Expose as `window.translatejs` in browsers

--- index.js
+++ index.js
@@ -33,113 +33,122 @@
  */
 
 /* global console, module */
+(function(){
+  'use strict';
 
-'use strict';
-
-var isObject = function(obj) {
-  return obj  &&  typeof obj === 'object';
-};
-
-
-module.exports = function(messageObject, options) {
-  var debug = options && options.debug;
-
-  function getTranslationValue(translationKey) {
-    var transValue = tFunc.keys[translationKey];
-    if( transValue == null ) {
-      var path = translationKey.split( (tFunc.opts && tFunc.opts.namespaceSplitter) || '::' );
-      var i = 0;
-      var len = path.length;
-      if ( len > 1 )
-      {
-        // Start walking
-        transValue = tFunc.keys;
-        while ( len > i ) {
-          transValue = transValue[ path[i] || '' ];
-          if ( transValue == null ) {
-            break;
-          }
-          i++;
-        }
-      }
-    }
-    return transValue;
-  }
-
-
-  function getPluralValue(translation, count) {
-    // Opinionated assumption: Pluralization rules are the same for negative and positive values.
-    // By normalizing all values to positive, pluralization functions become simpler, and less error-prone by accident.
-    var mappedCount = Math.abs(count);
-
-    if(translation[mappedCount] != null){
-      translation = translation[mappedCount];
-    } else {
-      var plFunc = (tFunc.opts||{}).pluralize;
-      mappedCount = plFunc ? plFunc( mappedCount, translation ) : mappedCount;
-      if(translation[mappedCount] != null){
-        translation = translation[mappedCount];
-      } else if(translation.n != null) {
-        translation = translation.n;
-      } else {
-        debug && console.warn('No plural forms found for "' + count + '" in', translation);
-      }
-    }
-    return translation;
-  }
-
-  function replacePlaceholders(translation, replacements, count) {
-    return translation.replace(/\{(\w*)\}/g, function (match, key) {
-      var val = replacements[key];
-      if (!replacements.hasOwnProperty(key)) {
-        if ( key === 'n' || count != null) {
-          val = count;
-        } else {
-          debug && console.warn('No "' + key + '" in placeholder object:', replacements);
-          val = match;
-        }
-      }
-      return val;
-    });
-  }
-
-  var tFunc = function (translationKey, count, replacements) {
-    var translation = getTranslationValue(translationKey);
-    var complex = arguments.length > 1;
-
-    if ( complex )
-    {
-      if ( isObject(count) ) {
-        var tmp = replacements;
-        replacements = count;
-        count = tmp;
-      }
-      replacements = replacements || {};
-      count = typeof count === 'number' ? count : null;
-
-
-      if ( count != null && isObject(translation) ) {
-        //get appropriate plural translation string
-        translation = getPluralValue(translation, count);
-      }
-    }
-
-    if ( typeof translation !== 'string' ) {
-      translation = translationKey;
-      if (debug) {
-          translation = '@@' + translation + '@@';
-          console.warn('Translation for "' + translationKey + '" not found.');
-      }
-    } else if ( complex || debug ) {
-      translation = replacePlaceholders(translation, replacements, count);
-    }
-
-    return translation;
+  var isObject = function(obj) {
+    return obj  &&  typeof obj === 'object';
   };
 
-  tFunc.keys = messageObject || {};
-  tFunc.opts = options || {};
+  var translatejs = function(messageObject, options) {
+
+    var debug = options && options.debug;
+
+    function getTranslationValue(translationKey) {
+      var transValue = tFunc.keys[translationKey];
+      if( transValue == null ) {
+        var path = translationKey.split( (tFunc.opts && tFunc.opts.namespaceSplitter) || '::' );
+        var i = 0;
+        var len = path.length;
+        if ( len > 1 )
+        {
+          // Start walking
+          transValue = tFunc.keys;
+          while ( len > i ) {
+            transValue = transValue[ path[i] || '' ];
+            if ( transValue == null ) {
+              break;
+            }
+            i++;
+          }
+        }
+      }
+      return transValue;
+    }
 
 
-  return tFunc;
-};
+    function getPluralValue(translation, count) {
+      // Opinionated assumption: Pluralization rules are the same for negative and positive values.
+      // By normalizing all values to positive, pluralization functions become simpler, and less error-prone by accident.
+      var mappedCount = Math.abs(count);
+
+      if(translation[mappedCount] != null){
+        translation = translation[mappedCount];
+      } else {
+        var plFunc = (tFunc.opts||{}).pluralize;
+        mappedCount = plFunc ? plFunc( mappedCount, translation ) : mappedCount;
+        if(translation[mappedCount] != null){
+          translation = translation[mappedCount];
+        } else if(translation.n != null) {
+          translation = translation.n;
+        } else {
+          debug && console.warn('No plural forms found for "' + count + '" in', translation);
+        }
+      }
+      return translation;
+    }
+
+    function replacePlaceholders(translation, replacements, count) {
+      return translation.replace(/\{(\w*)\}/g, function (match, key) {
+        var val = replacements[key];
+        if (!replacements.hasOwnProperty(key)) {
+          if ( key === 'n' || count != null) {
+            val = count;
+          } else {
+            debug && console.warn('No "' + key + '" in placeholder object:', replacements);
+            val = match;
+          }
+        }
+        return val;
+      });
+    }
+
+    var tFunc = function (translationKey, count, replacements) {
+      var translation = getTranslationValue(translationKey);
+      var complex = arguments.length > 1;
+
+      if ( complex )
+      {
+        if ( isObject(count) ) {
+          var tmp = replacements;
+          replacements = count;
+          count = tmp;
+        }
+        replacements = replacements || {};
+        count = typeof count === 'number' ? count : null;
+
+
+        if ( count != null && isObject(translation) ) {
+          //get appropriate plural translation string
+          translation = getPluralValue(translation, count);
+        }
+      }
+
+      if ( typeof translation !== 'string' ) {
+        translation = translationKey;
+        if (debug) {
+            translation = '@@' + translation + '@@';
+            console.warn('Translation for "' + translationKey + '" not found.');
+        }
+      } else if ( complex || debug ) {
+        translation = replacePlaceholders(translation, replacements, count);
+      }
+
+      return translation;
+    };
+
+    tFunc.keys = messageObject || {};
+    tFunc.opts = options || {};
+
+
+    return tFunc;
+  };
+
+
+  if ( typeof module !== 'undefined' && module.exports ) {
+    module.exports = translatejs;
+  } else {
+    window.translatejs = translatejs;
+  }
+
+})();
