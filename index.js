cd41aa3f7f3cccf7b94c2bdecc7a06088e8ccb4e
Evren Ozkan <e.ozkan@plaxis.com>
2018-04-19 10:13:55 +0200

    Formatting.

--- index.js
+++ index.js
@@ -141,7 +141,9 @@
             console.warn('Translation for "' + translationKey + '" not found.')
           }
         }
-      } if (complex || debug) {
+      }
+
+      if (complex || debug) {
         translation = replacePlaceholders(translation, replacements, subKey)
       }
       return translation
